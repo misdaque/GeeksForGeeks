aaaae2f755c89eb9c7a6ca9ed9425dbfd00ac371
misdaque <misdaque.azdee960@gmai.com>
2022-09-24 10:16:51 +0530

    Heap Insert and Delete

--- DataStructure/Heap/HeapImplementation.java
+++ DataStructure/Heap/HeapImplementation.java
@@ -9,7 +9,16 @@ class HeapImplementation{
       System.out.println();
 
       for(int i = 1; i < arr.length ; i++)
-        maxHeapInsert(arr, i);
+        maxHeapInsertRevision(arr, i);
+
+      System.out.print("Max Heap is: ");
+
+      for(int i : arr)
+        System.out.print(i + " ");
+
+      delete(arr, arr.length - 1);
+
+      System.out.println();
 
       System.out.print("Max Heap is: ");
 
@@ -22,7 +31,7 @@ class HeapImplementation{
     int temp = arr[i];
     int prev = i;
 
-    while(i >=0){
+    while(i >= 0){
       i = i % 2 == 0 ? (i - 1) / 2 : (i - 2) / 2;
 
       if(temp > arr[i]){
@@ -32,6 +41,62 @@ class HeapImplementation{
       } else{
         break;
       }
+    }
+  }
+
+  private static void maxHeapInsertRevision(int[] arr, int i){
+    int temp = arr[i];
+    int prev = i;
+
+    while(i >= 0){
+
+      i = (i & 1) != 0 ? (i - 2) / 2 : (i - 1) / 2;
+
+      if(temp > arr[i]){
+        arr[prev] = arr[i];
+        arr[i] = temp;
+      } else
+        break;
+
+        prev = i;
+    }
+  }
+
+  //TODO: Not working the way expected
+  private static void delete(int[] arr, int h){
+    int i = 0;
+    int deleted = arr[0];
+
+    arr[0] = arr[h];
+    arr[h] = deleted;
+
+    h--;
+
+    int lc = 2 * i + 1;
+    int rc = lc + 1;
+
+    int greaterIndex = -1;
+
+    while(true){
+      if(rc <= h)
+        greaterIndex = arr[rc] > arr[lc] ? rc : lc;
+      else if(lc <= h)
+        greaterIndex = lc;
+      else
+        return;
+
+      if(arr[greaterIndex] > arr[i]){
+        arr[greaterIndex] = arr[greaterIndex] ^ arr[i];
+        arr[i] = arr[greaterIndex] ^ arr[i];
+        arr[greaterIndex] = arr[greaterIndex] ^ arr[i];
+      }
+      else
+        return;
+
+      i = greaterIndex;
+
+      lc = 2 * i + 1;
+      rc = lc + 1;
     }
   }
 }
